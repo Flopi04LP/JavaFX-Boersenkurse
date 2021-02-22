92c1369c86ab7d3bb8574b0d64733068a0249395
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-02-22 10:17:53 +0100

    Add files via upload

--- Boersenkurse/src/ch/floundsimon/ch/boerse/FXMLDocumentController.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package ch.floundsimon.ch.boerse;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +16,7 @@ import javafx.scene.chart.Chart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -30,19 +32,58 @@ public class FXMLDocumentController implements Initializable {
     private NumberAxis y;
     @FXML
     private CategoryAxis x;
+    @FXML
+    private Button btnbitcoin;
+    @FXML
+    private Button btnethereum;
+    @FXML
+    private Button btndogecoin;
+    @FXML
+    private Button btnstocks;
     
     
     
         
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        XYChart.Series<String, Number> data = new XYChart.Series<>();        
-        data.getData().add(new XYChart.Data<>("1995", 180000));
-        data.getData().add(new XYChart.Data<>("1996", 190000));
-        data.getData().add(new XYChart.Data<>("1997", 240000));
-        data.getData().add(new XYChart.Data<>("1998", 230000));
+        
+        
+        
+    }    
+
+    @FXML
+    private void btnclickbitcoin(ActionEvent event) {
+        
+        XYChart.Series<String, Number> databit = new XYChart.Series<>();   
+        databit.getData().removeAll(Collections.singleton(chart.getData().setAll()));
+        databit.getData().add(new XYChart.Data<>("1995", 180000));
+        databit.getData().add(new XYChart.Data<>("1996", 190000));
+        databit.getData().add(new XYChart.Data<>("1997", 240000));
+        databit.getData().add(new XYChart.Data<>("1998", 230000));
+        
+        chart.getData().add(databit);
+    }
+
+    @FXML
+    private void btnclickethereum(ActionEvent event) {
+        XYChart.Series<String, Number> data = new XYChart.Series<>(); 
+        
+        data.getData().removeAll(Collections.singleton(chart.getData().setAll()));
+               
+        data.getData().add(new XYChart.Data<>("1995", 18000));
+        data.getData().add(new XYChart.Data<>("1996", 19000));
+        data.getData().add(new XYChart.Data<>("1997", 24000));
+        data.getData().add(new XYChart.Data<>("1998", -23));
         
         chart.getData().add(data);
-    }    
+    }
+
+    @FXML
+    private void btnclickdogecoin(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnclickstocks(ActionEvent event) {
+    }
     
 }
