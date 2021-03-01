6fb36a3c62f5a75b40490a98e2a00633d03229e5
simon <kappelers77@gmail.com>
2021-03-01 22:29:36 +0100

    Update Starter.java

--- Boersenkurse/src/ch/floundsimon/ch/boerse/Starter.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/Starter.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.floundsimon.ch.boerse;
 
 import javafx.application.Application;
@@ -14,7 +9,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author flori
+ * @author Florian Büchi & Simon Kappeler
  */
 public class Starter extends Application {
 
@@ -23,20 +18,13 @@ public class Starter extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
             Scene scene = new Scene(root);
-
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             System.out.println(e);
-        }
-
-        
-
+        }    
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
