f7eee0f6cc186447e23ed582a38cf5d9073fa8e9
simon <kappelers77@gmail.com>
2021-03-01 08:47:56 +0100

    Update Data.java

--- Boersenkurse/src/ch/floundsimon/ch/boerse/Data.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/Data.java
@@ -27,8 +27,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Data {
-    private double eth, doge, btc =0;
-    
+
+    private double eth, doge, btc = 0;
+
     ArrayList<Double> btcArray = new ArrayList<>();
 
     public void getData() {
@@ -40,7 +41,12 @@ public class Data {
             String result = makeAPICall(uri, paratmers);
             System.out.println(result);
         } catch (Exception e) {
-            System.out.println("Error: cannont access content - " + e.toString());
+            System.out.println("Error while making the api call");
+        }
+        try{
+            ArrayList<Double> btcArray = getPriceChange(Coins.BITCOIN);
+        } catch(Exception e){
+            
         }
     }
 
@@ -63,12 +69,12 @@ public class Data {
             HttpEntity entity = response.getEntity();
             response_content = EntityUtils.toString(entity);
             EntityUtils.consume(entity);
-
             FileWriter fileWriter = new FileWriter("a.json");
             PrintWriter printWriter = new PrintWriter(fileWriter);
             printWriter.print(response_content);
             printWriter.close();
-
+        }catch (Exception e){
+            System.out.println(e);
         } finally {
             response.close();
         }
@@ -76,30 +82,73 @@ public class Data {
         return response_content;
     }
 
+    public void writeJsons() throws FileNotFoundException, IOException, org.json.simple.parser.ParseException {
 
-    public void writeJsons() throws FileNotFoundException, IOException, org.json.simple.parser.ParseException{
-        
         JSONParser parser = new JSONParser();
         Object object = parser.parse(new FileReader("a.json"));
-        
+
         JSONArray array = (JSONArray) object;
-        
+
         JSONObject btcJsonObj = (JSONObject) array.get(0);
         String btcString = (String) btcJsonObj.get("price");
-        System.out.println("Price BTC = "+btcString);
+
         btc = Double.valueOf(btcString);
-        
-        
+
+        System.out.println("Price BTC = " + btcString);
+
         JSONObject ethJsonObj = (JSONObject) array.get(1);
         String ethString = (String) ethJsonObj.get("price");
-        System.out.println("Price ETH = "+ethString);
+        System.out.println("Price ETH = " + ethString);
         eth = Double.valueOf(ethString);
-        
-        
+
         JSONObject dogeJsonObj = (JSONObject) array.get(2);
         String dogeString = (String) dogeJsonObj.get("price");
-        System.out.println("Price DOGE = "+dogeString);
+        System.out.println("Price DOGE = " + dogeString);
         doge = Double.valueOf(dogeString);
+    }
+
+    public ArrayList<Double> getPriceChange(Coins coin) throws FileNotFoundException, IOException, org.json.simple.parser.ParseException {
+        JSONParser parser = new JSONParser();
+        Object object = parser.parse(new FileReader("a.json"));
+
+        JSONArray array = (JSONArray) object;
+        JSONObject jsonObj = null;
+        switch (coin) {
+            case BITCOIN:
+                jsonObj = (JSONObject) array.get(0);
+                break;
+            case ETHEREUM:
+                jsonObj = (JSONObject) array.get(1);
+                break;
+            case DOGECOIN:
+                jsonObj = (JSONObject) array.get(2);
+                break;
+
+        }
+
+        JSONObject hourObj = (JSONObject) jsonObj.get("1h");
+        Object hourO = hourObj.get("price_change");  
+        JSONObject dayObj = (JSONObject) jsonObj.get("1d");
+        Object dayO = dayObj.get("price_change");
+        JSONObject weekObj = (JSONObject) jsonObj.get("7d");
+        Object weekO = weekObj.get("price_change");
+        JSONObject monthObj = (JSONObject) jsonObj.get("30d");
+        Object monthO =  monthObj.get("price_change");
+        
+        Double hour = Double.valueOf(hourO.toString());
+        Double day = Double.valueOf(dayO.toString());
+        Double week = Double.valueOf(weekO.toString());
+        Double month = Double.valueOf(monthO.toString());
+        
+        
+        ArrayList<Double> returnArray = new ArrayList<>();
+        System.out.println(hour);
+        returnArray.set(0, hour);
+        returnArray.set(1, day);
+        returnArray.set(2, week);
+        returnArray.set(3, month);
+        System.out.println(returnArray);
+        return returnArray;
     }
 
     public double getEth() {
@@ -113,5 +162,65 @@ public class Data {
     public double getBtc() {
         return btc;
     }
+
+    public ArrayList<Double> getBtcPriceChange(Coins coin) throws org.json.simple.parser.ParseException, IOException{
+        return getPriceChange(coin);
+    }
     
+    public Double getStock(String stock) 
+            throws URISyntaxException, IOException, ParseException, org.json.simple.parser.ParseException{
+                
+        
+        String uri = "https://finnhub.io/api/v1/quote?symbol="+stock+"&token=sandbox_c0u914748v6qqphtv320";
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(params);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            // System.out.println(response.getStatusLine());
+
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+            FileWriter fileWriter = new FileWriter("stock.json");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.print(response_content);
+            printWriter.close();
+        }catch (Exception e){
+            System.out.println(e);
+        } finally {
+            response.close();
+        }
+        
+        System.out.println("Price "+stock+":  "+writeStockJson());
+        return writeStockJson();
+    }
+    private Double writeStockJson() throws FileNotFoundException, IOException, org.json.simple.parser.ParseException{
+    JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(new FileReader("stock.json"));
+        Object a = object.get("c");
+        
+        String priceStr = String.valueOf(a);
+        Double price;
+        
+        try{
+            price = Double.valueOf(priceStr);
+            
+        } catch(Exception e){
+            price = 111.0;
+        }
+        
+        return price;
+}
+}
+
 }
