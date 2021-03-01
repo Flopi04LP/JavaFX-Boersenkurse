9483946650299ecfde0aec75107e388850e8d3c9
simon <kappelers77@gmail.com>
2021-03-01 08:47:35 +0100

    Update FXMLDocumentController.java

--- Boersenkurse/src/ch/floundsimon/ch/boerse/FXMLDocumentController.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/FXMLDocumentController.java
@@ -1,10 +1,16 @@
 package ch.floundsimon.ch.boerse;
 
+import static ch.floundsimon.ch.boerse.Coins.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +22,7 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -54,36 +61,42 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+        try {
+            start();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void btnclickbitcoin(ActionEvent event) throws IOException, FileNotFoundException, ParseException {
         start();
-    }
-
-    @FXML
-    private void btnclickbitcoin(ActionEvent event) {
-        start();
 
     }
 
     @FXML
-    private void btnclickethereum(ActionEvent event) {
+    private void btnclickethereum(ActionEvent event) throws IOException, FileNotFoundException, ParseException {
 
-        klicked("ETHEREUM");
+        klicked(ETHEREUM);
     }
 
     @FXML
-    private void btnclickdogecoin(ActionEvent event) {
-        klicked("DOGECOIN");
+    private void btnclickdogecoin(ActionEvent event) throws IOException, FileNotFoundException, ParseException {
+        klicked(DOGECOIN);
     }
 
     @FXML
     private void btnclickstocks(ActionEvent event) {
     }
 
-    public void start() {
-        klicked("BITCOIN");
+    public void start() throws IOException, FileNotFoundException, ParseException {
+        klicked(BITCOIN);
     }
 
-    private void klicked(String coin) {
-        XYChart.Series<String, Number> databit = new XYChart.Series<>();
+    private void klicked(Coins coin) throws IOException, FileNotFoundException, ParseException {
+         XYChart.Series<String, Number> databit = new XYChart.Series<>();
         databit.getData().removeAll(Collections.singleton(chart.getData().setAll()));
         Data data = new Data();
         int vals[] = new int[10];
@@ -95,12 +108,10 @@ public class FXMLDocumentController implements Initializable {
                     databit.getData().add(new XYChart.Data<>(String.valueOf(i), (int)data.getBtc()));
                     vals[i] = (int) data.getBtc();
                     break;
-
                 case "ETHEREUM":
                     databit.getData().add(new XYChart.Data<>(String.valueOf(i), (int)data.getEth()));
                     vals[i] = (int) data.getEth();
                     break;
-
                 case "DOGECOIN":
                     databit.getData().add(new XYChart.Data<>(String.valueOf(i), (int)data.getDoge()));
                     vals[i] = (int) data.getDoge();
@@ -108,13 +119,18 @@ public class FXMLDocumentController implements Initializable {
                     
             }
 */
-            
+            /*
             Random r = new Random();
             int rnd = r.nextInt(10) + 50;
             databit.getData().add(new XYChart.Data<>(String.valueOf(i), rnd));
-            vals[i] = rnd;            
+            vals[i] = rnd;       
+            */
         }
+       
 
+                        
+            
+            
         // System.out.println(vals[5] + "  "+ vals[6]);
         gainsPerc = Double.valueOf(df.format(calcPerc(vals[6], vals[5])));
 
@@ -129,7 +145,7 @@ public class FXMLDocumentController implements Initializable {
         }
 
         chart.getData().add(databit);
-        chart.setTitle(coin);
+        chart.setTitle(coin.toString(coin));
     }
 
     private double calcPerc(double vorher, double nacher) {
@@ -140,5 +156,4 @@ public class FXMLDocumentController implements Initializable {
         return a;
     }
 }
-
 
