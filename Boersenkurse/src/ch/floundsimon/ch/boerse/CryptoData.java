a2f1c8c16c61d2d5f9c2730a61c21e08a1cc2468
simon <kappelers77@gmail.com>
2021-03-02 21:55:09 +0100

    Update CryptoData.java

--- Boersenkurse/src/ch/floundsimon/ch/boerse/CryptoData.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/CryptoData.java
@@ -3,22 +3,11 @@ package ch.floundsimon.ch.boerse;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.HttpHeaders;
-import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.net.URIBuilder;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 /**
  *
- * @author kappe
+ * @author Florian Büchi & Simon Kappeler
  */
 public class CryptoData {
 
