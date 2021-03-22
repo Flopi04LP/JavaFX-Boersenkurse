107daa2229e3d42a6126a748dcd83210e15c92a2
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-03-22 09:10:29 +0100

    Update Starter.java

--- Boersenkurse/src/ch/floundsimon/ch/boerse/Starter.java
+++ Boersenkurse/src/ch/floundsimon/ch/boerse/Starter.java
@@ -20,6 +20,7 @@ public class Starter extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
+            stage.setTitle("Börsenkurse");
             stage.show();
         } catch (Exception e) {
             System.out.println(e);
